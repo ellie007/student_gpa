49853e3c8de1e6db065d6791cca4085b765ffaac
Eleanor Lee <ellie007@gmail.com>
2016-05-30 18:13:44 -0500

    delete classes resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  
+
   root 'students#index'
   resources :students
-  resources :classes
+
 # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"

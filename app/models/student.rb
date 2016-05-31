73682168bd4c967a558bd930f717ca88c53b18a1
Eleanor Lee <ellie007@gmail.com>
2016-05-31 11:47:11 -0500

    add student klass grade helper method

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ActiveRecord::Base
 
   has_many :student_classes
   has_many :klasses, through: :student_classes
-  accepts_nested_attributes_for :student_classes
+  accepts_nested_attributes_for :student_classes #is this necessary?
 
   validates :email, uniqueness: true
 
@@ -12,6 +12,10 @@ class Student < ActiveRecord::Base
       gpa += klass.grade
     end
     (gpa / self.student_classes.count).round(1)
+  end
+
+  def grade_for_klass(klass)
+    self.student_classes.find_by_klass_id(klass.id).grade
   end
 
 end
